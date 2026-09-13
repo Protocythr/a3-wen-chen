961905c9e1edec82a9a86b37d6fa2fd5444ea95b
Wen Chen <you@example.com>
2026-09-13 15:06:07 -0400

    finisted baseplate

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 import express from 'express';
 const app = express();
+import path from 'path';
+import { fileURLToPath } from "url";
 const port = 3000;
 
 import { runGetStarted } from './public/index.js';
 
+// Create __dirname for ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Serve files from the public folder
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get('/', (req, res) => {
-    res.send('Hello World!');
     runGetStarted();
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening at http://localhost:${port}`);
 });
 
 app.use(express.static('public'));
