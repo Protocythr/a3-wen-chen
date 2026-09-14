2fa3f90c2a5c6364b1d52e7861f9947d807722f3
Wen Chen <you@example.com>
2026-09-13 22:13:33 -0400

    added server corespondence

--- server.js
+++ server.js
@@ -13,6 +13,27 @@ const __dirname = path.dirname(__filename);
 // Serve files from the public folder
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/api/submit-expense', (req, res) => {
+    const json = {
+        expenseDate: req.body.date,
+        expenseName: req.body.expense,
+        expenseAmount: req.body.amount,
+        expenseCategory: req.body.category,
+        expenseDescription: req.body.description
+    };
+
+    console.log("Data received on server:", req.body);
+
+    // Send a response back to the client
+    res.status(200).json({
+        status: "success",
+        message: "Data successfully received!",
+    });
+});
+
 app.get('/', (req, res) => {
     runGetStarted();
 });
